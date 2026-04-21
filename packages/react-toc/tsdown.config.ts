6253a6c4d805b197350123a004f61999ce07c6e4
HyunJinNo <nhj9831@naver.com>
2026-04-21 16:09:24 +0900

    build: tsdown 번들러 설정

--- packages/react-toc/tsdown.config.ts
+++ packages/react-toc/tsdown.config.ts
@@ -3,7 +3,13 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
   entry: ["./src/index.ts", "./src/heading.ts"],
   format: ["esm", "cjs"],
+  outDir: "./dist",
   dts: true,
   clean: true,
+  treeshake: true,
+  minify: true,
+  css: {
+    minify: true,
+  },
   deps: { neverBundle: ["react", "react-dom"] },
 });
